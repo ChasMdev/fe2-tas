622dede2fd425766df92170a3a4e87f708eaba41
ChasM <litejett@gmail.com>
2024-09-06 00:09:59 +0100

    small update

---
+++ src/GUI/Lobby/Recent/CreateListPanel.tsx
@@ -0,0 +1,14 @@
+import Roact from "@rbxts/roact";
+
+export function createListPanel(index: number) {
+	let panelSize = 32;
+	if (index === 1) {
+		panelSize = 41;
+	} else {
+		panelSize = 32;
+	}
+
+	const panel = <frame></frame>;
+
+	return panel;
+}
