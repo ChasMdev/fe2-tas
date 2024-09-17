0bc484a5dc60e80987b39c335b67443a33f6bde7
ChasM <litejett@gmail.com>
2024-09-17 23:52:06 +0100

    update GUI (still not finished crying emoji ;( 😭😭😭)

--- src/GUI/Lobby/Recent/CreateListPanel.tsx
+++ src/GUI/Lobby/Recent/CreateListPanel.tsx
@@ -1,119 +1,92 @@
 import Roact from "@rbxts/roact";
 
-export function createListPanel(
-	index: number,
-	mapName: string,
-	creationDate: string,
-	modifiedDate: string,
-	complete: number,
-) {
-	let panelSize = 32;
-	let panelColor = "#0E1014";
-	let corner = 0;
-	if (index === 1) {
-		panelSize = 41;
-		corner = 6;
-	}
+export function createListPanel(index: number, fileName: string, modifiedDate: string, fileID: string) {
+	let panelColor = "#1E1F24";
 
 	if (index % 2 === 0) {
-		panelColor = "#1A1C22";
+		panelColor = "#101418";
 	}
 
 	const panel = (
 		<frame
 			Key={"Panel"}
-			Size={new UDim2(1, 0, 0, panelSize)}
-			Position={new UDim2(0, 0, 0, (index - 1) * 32)}
+			Size={new UDim2(1, 0, 0.123, 0)}
+			Position={new UDim2(0, 0, 0.123 * (index - 1), 0)}
 			BackgroundColor3={Color3.fromHex(panelColor)}
-			ZIndex={index}
+			LayoutOrder={index}
 		>
-			<textlabel
-				Key={"MapName"}
-				Size={new UDim2(0.106, 0, 0, 16)}
-				Position={new UDim2(0.012, 0, 0, 8)}
-				Text={mapName}
-				FontFace={new Font("rbxassetid://12187365364", Enum.FontWeight.SemiBold)}
-				TextColor3={Color3.fromHex("#EBEBEB")}
+			<uicorner CornerRadius={new UDim(0, 6)} />
+			<imagebutton
+				Key={"Extras"}
+				AnchorPoint={new Vector2(0, 0.5)}
 				BackgroundTransparency={1}
-				TextSize={13}
-				TextXAlignment={"Left"}
-				ZIndex={index}
+				Size={new UDim2(0.04, 0, 0.624, 0)}
+				Position={new UDim2(0.012, 0, 0.5, 0)}
+				Image={"rbxassetid://77800512493780"}
+				ImageColor3={Color3.fromHex("#C1C6CA")}
+			></imagebutton>
+			<textlabel
+				Key={"Index"}
+				BackgroundTransparency={1}
+				AnchorPoint={new Vector2(0, 0.5)}
+				Size={new UDim2(0.064, 0, 0.374, 0)}
+				Position={new UDim2(0.074, 0, 0.5, 0)}
+				FontFace={new Font("rbxassetid://12187365364", Enum.FontWeight.Bold)}
+				Text={`#${index}`}
+				TextScaled={true}
+				TextColor3={Color3.fromHex("#3D3F43")}
 			/>
 			<textlabel
-				Key={"CreationDate"}
-				Size={new UDim2(0.106, 0, 0, 16)}
-				Position={new UDim2(0.365, 0, 0, 8)}
-				Text={creationDate}
-				FontFace={new Font("rbxassetid://12187365364", Enum.FontWeight.SemiBold)}
-				TextColor3={Color3.fromHex("#EBEBEB")}
+				Key={"fileName"}
 				BackgroundTransparency={1}
-				TextSize={13}
-				TextXAlignment={"Left"}
-				ZIndex={index}
+				AnchorPoint={new Vector2(0, 0.5)}
+				Size={new UDim2(0.383, 0, 0.35, 0)}
+				Position={new UDim2(0.167, 0, 0.5, 0)}
+				FontFace={new Font("rbxassetid://12187365364", Enum.FontWeight.SemiBold)}
+				Text={`${fileName}`}
+				TextScaled={true}
+				TextColor3={Color3.fromHex("#C1C6CA")}
 			/>
 			<textlabel
-				Key={"ModifiedDate"}
-				Size={new UDim2(0.1, 0, 0, 16)}
-				Position={new UDim2(0.5, 0, 0, 8)}
-				Text={modifiedDate}
-				FontFace={new Font("rbxassetid://12187365364", Enum.FontWeight.SemiBold)}
-				TextColor3={Color3.fromHex("#EBEBEB")}
+				Key={"modifiedDate"}
 				BackgroundTransparency={1}
-				TextSize={13}
-				TextXAlignment={"Left"}
-				ZIndex={index}
-			/>
-			<textlabel
-				Key={"Completed"}
-				Size={new UDim2(0.12, 0, 0, 16)}
-				Position={new UDim2(0.667, 0, 0, 8)}
-				Text={`${complete}% Complete`}
+				AnchorPoint={new Vector2(0, 0.5)}
+				Size={new UDim2(0.163, 0, 0.35, 0)}
+				Position={new UDim2(0.564, 0, 0.5, 0)}
 				FontFace={new Font("rbxassetid://12187365364", Enum.FontWeight.SemiBold)}
-				TextColor3={Color3.fromHex("#EBEBEB")}
-				BackgroundTransparency={1}
-				TextSize={13}
-				TextXAlignment={"Left"}
-				ZIndex={index}
+				Text={`${modifiedDate}`}
+				TextScaled={true}
+				TextColor3={Color3.fromHex("#C1C6CA")}
 			/>
-			<uicorner CornerRadius={new UDim(0, corner)} />
 			<textbutton
-				Key={"Delete"}
-				Size={new UDim2(0, 24, 0, 24)}
-				Position={new UDim2(0.927, 0, 0, 4)}
+				Key={"fileID"}
+				AnchorPoint={new Vector2(0, 0.5)}
+				BackgroundColor3={Color3.fromHex("#25272C")}
+				Position={new UDim2(0.756, 0, 0.5, 0)}
+				Size={new UDim2(0.174, 0, 0.79, 0)}
 				Text={""}
-				BackgroundColor3={Color3.fromHex("#14161B")}
-				ZIndex={index}
 			>
-				<uicorner CornerRadius={new UDim(0, 3)} />
-				<imagelabel
-					Key={"Icon"}
+				<uicorner CornerRadius={new UDim(0, 4)} />
+				<textlabel
 					AnchorPoint={new Vector2(0.5, 0.5)}
-					Size={new UDim2(0.833, 0, 0.833, 0)}
-					Position={new UDim2(0.5, 0, 0.5, 0)}
 					BackgroundTransparency={1}
-					Image={"rbxassetid://127086072451757"}
-					ZIndex={index}
+					Size={new UDim2(0.542, 0, 0.474, 0)}
+					Position={new UDim2(0.5, 0, 0.5, 0)}
+					FontFace={new Font("rbxassetid://12187365364", Enum.FontWeight.SemiBold)}
+					Text={"T-••••••••"}
+					TextScaled={true}
+					TextColor3={Color3.fromHex("#C1C6CA")}
 				/>
 			</textbutton>
-			<textbutton
+			<imagebutton
 				Key={"Select"}
-				Size={new UDim2(0, 24, 0, 24)}
-				Position={new UDim2(0.964, 0, 0, 4)}
-				Text={""}
-				BackgroundColor3={Color3.fromHex("#14161B")}
-				ZIndex={index}
-			>
-				<uicorner CornerRadius={new UDim(0, 3)} />
-				<imagelabel
-					Key={"Icon"}
-					AnchorPoint={new Vector2(0.5, 0.5)}
-					Size={new UDim2(0.833, 0, 0.833, 0)}
-					Position={new UDim2(0.5, 0, 0.5, 0)}
-					BackgroundTransparency={1}
-					Image={"rbxassetid://114751447659442"}
-					ZIndex={index}
-				/>
-			</textbutton>
+				AnchorPoint={new Vector2(0, 0.5)}
+				BackgroundTransparency={1}
+				Size={new UDim2(0.04, 0, 0.624, 0)}
+				Position={new UDim2(0.948, 0, 0.5, 0)}
+				Image={"rbxassetid://114751447659442"}
+				ImageColor3={Color3.fromHex("#C1C6CA")}
+			/>
 		</frame>
 	);
 
